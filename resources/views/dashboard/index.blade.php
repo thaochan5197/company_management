2f31cad3891ab2925045d48de3730338de6a0abd
unknown <TruongNV>
2019-07-04 18:06:19 +0700

    first commit

---
+++ resources/views/dashboard/index.blade.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: TruongNv
+ * Date: 2019-07-04
+ * Time: 3:14 PM
+ */
+?>
+@extends('layouts.admin')
---
+++ resources/views/layouts/admin.blade.php
@@ -0,0 +1,82 @@
+<!doctype html>
+<html class="no-js" lang="en">
+
+
+<!-- Mirrored from demo.hasthemes.com/adomx-preview/light/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 04 Jul 2019 07:23:23 GMT -->
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Adomx - Responsive Bootstrap 4 Admin Template</title>
+    <meta name="robots" content="noindex, follow" />
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Favicon -->
+    <link rel="shortcut icon" type="image/x-icon" href="assets/images/favicon.ico">
+
+    <!-- CSS
+	============================================ -->
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="assets/css/vendor/bootstrap.min.css">
+
+    <!-- Icon Font CSS -->
+    <link rel="stylesheet" href="assets/css/vendor/material-design-iconic-font.min.css">
+    <link rel="stylesheet" href="assets/css/vendor/font-awesome.min.css">
+    <link rel="stylesheet" href="assets/css/vendor/themify-icons.css">
+    <link rel="stylesheet" href="assets/css/vendor/cryptocurrency-icons.css">
+
+    <!-- Plugins CSS -->
+    <link rel="stylesheet" href="assets/css/plugins/plugins.css">
+
+    <!-- Helper CSS -->
+    <link rel="stylesheet" href="assets/css/helper.css">
+
+    <!-- Main Style CSS -->
+    <link rel="stylesheet" href="assets/css/style.css">
+
+    <!-- Custom Style CSS Only For Demo Purpose -->
+    <link id="cus-style" rel="stylesheet" href="assets/css/style-primary.css">
+
+</head>
+
+@yield('content')
+
+<body>
+<!-- JS
+============================================ -->
+
+<!-- Global Vendor, plugins & Activation JS -->
+<script src="assets/js/vendor/modernizr-3.6.0.min.js"></script>
+<script src="assets/js/vendor/jquery-3.3.1.min.js"></script>
+<script src="assets/js/vendor/popper.min.js"></script>
+<script src="assets/js/vendor/bootstrap.min.js"></script>
+<!--Plugins JS-->
+<script src="assets/js/plugins/perfect-scrollbar.min.js"></script>
+<script src="assets/js/plugins/tippy4.min.js.js"></script>
+<!--Main JS-->
+<script src="assets/js/main.js"></script>
+
+<!-- Plugins & Activation JS For Only This Page -->
+
+<!--Moment-->
+<script src="assets/js/plugins/moment/moment.min.js"></script>
+
+<!--Daterange Picker-->
+<script src="assets/js/plugins/daterangepicker/daterangepicker.js"></script>
+<script src="assets/js/plugins/daterangepicker/daterangepicker.active.js"></script>
+
+<!--Echarts-->
+<script src="assets/js/plugins/chartjs/Chart.min.js"></script>
+<script src="assets/js/plugins/chartjs/chartjs.active.js"></script>
+
+<!--VMap-->
+<script src="assets/js/plugins/vmap/jquery.vmap.min.js"></script>
+<script src="assets/js/plugins/vmap/maps/jquery.vmap.world.js"></script>
+<script src="assets/js/plugins/vmap/maps/samples/jquery.vmap.sampledata.js"></script>
+<script src="assets/js/plugins/vmap/vmap.active.js"></script>
+
+</body>
+
+
+<!-- Mirrored from demo.hasthemes.com/adomx-preview/light/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 04 Jul 2019 07:24:22 GMT -->
+</html>
